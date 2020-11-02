2d0419950e4421bb2162e53a3b72bb254a718b59
Borislove <abirme@yandex.ru>
2020-11-02 09:07:09 +0300

    страничка прокрастинации

---
+++ js/cicle.js
@@ -0,0 +1,12 @@
+
+for(let i= 0 ; i <=10; i++){
+    console.log("hello");
+    console.log(i);
+}
+
+
+let array = ['животные', 'растения', 'грибы', 'микроорганизмы', 'вирусы'];
+//перебор массива
+for(let i = 0 ; i <=array.length; i++){
+    console.log(array[i]);
+}
